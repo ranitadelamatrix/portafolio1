fc6616fadacafb53ecb84f14c402c15f797bed38
ranitadelamatrix <gastontrejo9099@hotmail.com>
2024-02-09 18:03:06 -0300

    "api pokemon"

--- api.js
+++ api.js
@@ -1,13 +1,28 @@
-const url = "https://pokeapi.co/api/v2/pokemon"
+const url = "https://pokeapi.co/api/v2/pokemon";
 
 fetch(url)
-.then(res =>res.json())
-.then(data =>{
-    const ruta = document.querySelector("script")
-    ruta.src = "api.js"
-    const nombres = data.results.map(n=>n.name).join('<br>');
-    const urlpo = data.results.map(n=>n.url)
-    const parrafo = document.getElementById("ponerApi")
-    parrafo.innerHTML= `nombre: ${nombres}` 
-    console.log(data.name)
-})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Hubo un problema al obtener los datos.');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const listaPokemons = document.getElementById("ponerApi");
+    const pokemons = data.results;
+
+    // Mapear cada Pokémon a un elemento <li> con un enlace
+    const listaHTML = pokemons.map(pokemon => `
+      <li>
+        <a href="https://pokeapi.co/api/v2/pokemon/${pokemon.name}">
+          ${pokemon.name}
+        </a>
+      </li>
+    `).join('');
+
+    // Agregar la lista de Pokémon al contenedor
+    listaPokemons.innerHTML = listaHTML;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
