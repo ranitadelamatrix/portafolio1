1052000805c407a23863cf52daf8a87648f20ffe
ranitadelamatrix <gastontrejo9099@hotmail.com>
2024-02-09 17:12:18 -0300

    "apilocal"

--- api.js
+++ api.js
@@ -1,10 +1,13 @@
-const url = "http://localhost:3000/libros"
+const url = "https://pokeapi.co/api/v2/pokemon"
 
 fetch(url)
 .then(res =>res.json())
 .then(data =>{
-    const libros = data.map(l =>l.titulo).join(' ,')
-    const parrafo = document.getElementById('ponerApi')
-    parrafo.innerHTML= `Libros: ${libros}`
-    console.log(libros)
+    const ruta = document.querySelector("script")
+    ruta.src = "api.js"
+    const nombres = data.results.map(n=>n.name).join('<br>');
+    const urlpo = data.results.map(n=>n.url)
+    const parrafo = document.getElementById("ponerApi")
+    parrafo.innerHTML= `nombre: ${nombres}` 
+    console.log(data.name)
 })
