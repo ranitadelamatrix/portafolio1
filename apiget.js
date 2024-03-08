44f7b9c6b109a3802dabe40b5c1e652201931b65
ranitadelamatrix <gastontrejo9099@hotmail.com>
2024-03-08 09:28:15 -0300

    "listado"

--- apiget.js
+++ apiget.js
@@ -5,13 +5,12 @@ document.getElementById('buscarIntegranteForm').addEventListener('submit', async
 
     try {
         const response = await fetch(`https://apiexpress-berd.onrender.com/${id}`);
-
+        const data = await response.json();
+        const parrafo = document.getElementById('resultado')
         if (!response.ok) {
+            parrafo.innerHTML = `no se pudo encontrar integrantes`
             throw new Error('No se pudo encontrar al integrante');
         }
-
-        const data = await response.json();
-        const parrafo = document.querySelector('p')
         parrafo.innerHTML =  `
         Nombre: ${data.nombre}<br> edad: ${data.edad}<br> email: ${data.email}`
         // Aquí puedes utilizar los datos obtenidos para mostrarlos en tu página
--- verifi.js
+++ verifi.js
@@ -8,3 +8,53 @@ function verificar(){
         console.log("error ")
     }
 }
+function integrantes(){
+  const url = 'https://apiexpress-berd.onrender.com/integrantes'
+  fetch(url)
+  .then(res =>{
+    if(!res.ok){
+      throw new Error("error al obtener integrantes")
+    } return res.json()
+  })
+  .then(data =>{
+    
+    const id = data.map(i=>i.id)
+    const nombre = data.map(n=>n.nombre)
+    
+    for (let i = 0; i < nombre.length; i++) {
+      const p = document.getElementById('todosIntegrantes')
+      const ul = document.createElement('ul')
+      const liNombre = document.createElement('li')
+      const liId = document.createElement('li')
+      
+      liNombre.textContent= `Nombre: ${nombre[i]}`
+      liId.textContent=` ID: ${id[i]}`
+      
+      ul.appendChild(liNombre)
+      //ul.appendChild(document.createTextNode('\u00A0\u00A0')) //agrega un espacio en blanco
+      ul.appendChild(liId)
+      p.appendChild(ul)
+//voy a continuar para ver una lista
+//p.innerHTML = `el nombre ${nombre[i]} con el id ${id[i]}`
+  //     console.log(`bien`);
+    }
+    
+
+    
+    
+    
+  })
+}
+
+document.getElementById("integranteForm").addEventListener("submit", function(event) {
+    document.getElementById("loading").style.display = "block";
+
+    // Simular una carga ficticia (reemplaza esto con tu lógica de carga real)
+    setTimeout(function() {
+      document.getElementById("loading").style.display = "none";
+      console.log("Carga exitosa");
+    }, 3000); // Tiempo de espera simulado de 3 segundos (puedes cambiar esto)
+
+    // Detener el envío del formulario real para este ejemplo
+    event.preventDefault();
+  });
